6e51ef8dfdfc1784a62ebf5ad42d0c45e95d82a6
Speloquin <speloqu24@gmail.com>
2021-02-28 11:20:17 -0700

    home page with language icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,47 @@ function Home() {
   return (
     <>
       <div className="jumbotron">
-        <div className="row">
-          <h3 className="col-md-9 greeting">
-            Hello, I'm Samantha. I develop and create interfaces that are fueled
-            by innovation.
-          </h3>
-          <p className="lead"></p>
-          <hr className="my-4" />
-        </div>
+        <img
+          class="textwrap rounded-circle"
+          id="blueHead"
+          src="./Assets/Social-Media-Icons/SPeloquinBlue.png"
+          alt="headshot"
+        />
+        <p className="rounded-circle">COLORADO BASED FULL STACK DEVELOPER</p>
+        <img
+          className="html skillsimage"
+          src="Assets/Social-Media-Icons/html5.png"
+          alt="html5"
+        />
+        <img
+          className="CSS3 skillsimage"
+          src="Assets/Social-Media-Icons/css3.png"
+          alt="css3"
+        />
+        <img
+          className="mern"
+          src="Assets/Social-Media-Icons/MERN-logo.png"
+          alt="jq"
+        />
+        <img
+          className="mysql skillsimage"
+          src="Assets/Social-Media-Icons/mysql-.png"
+          alt="mysql"
+        />
+        <img
+          className="npm skillsimage"
+          src="Assets/Social-Media-Icons/npm-logo.png"
+          alt="npm"
+        />
+        {/* <p className="lead"></p> */}
+        {/* <hr className="my-4" /> */}
       </div>
+      {/* <div className="skills">
+        <hr />
+        <h3>Technologies</h3>
+        <hr />
+        <
+      </div> */}
     </>
   );
 }
