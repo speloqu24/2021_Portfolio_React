b2b23a13ed40abc896eba2b1ea5f92ec2a69313b
Samantha Peloquin <70297318+speloqu24@users.noreply.github.com>
2021-03-19 10:20:10 -0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,11 @@ function About() {
     <>
       <div className="prow row">
         <div className="col abt">
-          <p className="abtp textwrap">
+          <p className="abtp">
             Thanks for coming by, my name is Samantha and I want to talk to you
             for a moment about why I think the creation of diverse applications
             are important.
+            <br />
             <br />
             The way technology is expanding these days our minds are growing and
             becoming more unique in approaches to solutions. With that being
@@ -20,20 +21,23 @@ function About() {
             where users are left scared of it, has me creating the applications
             I do.
             <br />
-            The worlds oldest technology was the chopping tool. The chopping
-            tool was carved from stone and used, as you guessed, to chop things.
-            These SIMPLE tools were created to accomplish the same task as a
-            sharp knife. Now they just look sexier. So many are intimidated by
-            technology because of the robustness of some of the gadgets, systems
-            and applications out there. I believe that's because they just don't
-            know the answer to the question What is the best and right way to do
-            what I need to do.
             <br />
-            As a developer I will take a goal oriented, custom approach that
-            will result in an easy to use and highly efficient application
-            without reinventing the chopping tool to do it.
+            It's a fun and appropriate challenge to make. The worlds oldest
+            technology was the chopping tool. The chopping tool was carved from
+            stone and used, as you guessed, to chop things. These SIMPLE tools
+            were created to accomplish the same task as a sharp knife. Now they
+            just look sexier. So many are intimidated by technology because of
+            the robustness of some of the gadgets, systems and applications out
+            there. I believe that's because they just don't know the answer to
+            the question What is the best and right way to do what I need to do.
+            <br />
+            <br />
+            My point is, as a developer I will take a goal oriented custom
+            approach that will result in an easy to use and highly efficient
+            application without reinventing the tools to do it.
           </p>
-  <div class="col">
+        </div>
+        <div class="col">
           <img
             src="./Assets/Social-Media-Icons/Personal/Beach.jpg"
             className="persImg"
@@ -55,11 +59,15 @@ function About() {
             className="persImg"
             alt="newzealand"
           />
-    <button className="btn btn-secondary" href="https://calendly.com/speloqu24"> Let's Talk </button>
+          <button
+            className="btn btn-secondary"
+            href="https://calendly.com/speloqu24"
+          >
+            Let's Talk
+          </button>
         </div>
       </div>
     </>
   );
 }
-
 export default About;
